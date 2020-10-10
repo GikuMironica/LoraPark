1fd8406220c48c9b921f096a4aaa9eb25ee92524
Braz Castana <castana@mail.hs-ulm.de>
2020-10-10 16:22:39 +0200

    [Theme] Theme class defined

--- lib/themes/lorapark_theme.dart
+++ lib/themes/lorapark_theme.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-// ignore: non_constant_identifier_names
-ThemeData LoraParkTheme() {
-  return ThemeData(
+class LoraParkTheme {
+  static ThemeData themeData = ThemeData(
     primarySwatch: Colors.blueGrey,
+    primaryColor: Color(0xFF121212),
+    primaryColorBrightness: Brightness.dark,
     accentColor: Colors.tealAccent,
     fontFamily: 'Hind',
     visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+
+  static BoxShadow boxShadow = BoxShadow(
+    color: Color(0xff657582).withOpacity(0.17),
+    blurRadius: 20,
+    spreadRadius: 2,
+    offset: Offset(5, 5),
   );
 }
