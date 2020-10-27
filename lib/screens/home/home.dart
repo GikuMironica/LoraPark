f33bc1aa3f47fe4f87cd129a69a6e76dfc891e1d
Uzezi Adherioma <uzeziadherioma@yahoo.com>
2020-10-27 14:37:18 +0100

    modified home page navigation to point to sensor list

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lorapark_app/screens/screens.dart';
+import 'package:lorapark_app/screens/sensor_list_page/sensor_list_page.dart';
 import 'package:lorapark_app/screens/widgets/lp_nav_bar/lp_nav_bar.dart';
 import 'package:lorapark_app/screens/widgets/lp_nav_bar/lp_nav_bar_item.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
@@ -44,7 +45,7 @@ class _HomePageState extends State<HomePage> {
   List<Widget> _buildScreens() {
     return [
       MapPage(),
-      SettingsPage(),
+      SensorListPage(),
       SettingsPage(),
     ];
   }
