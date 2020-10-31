7adfdce31a27afa677ac9f4a94dab2e38f56b474
krunkerista <rzayev.hidayat@gmail.com>
2020-10-31 22:10:58 +0100

    [Refactor] Moved sensor pages into one folder. Fixed some bugs.

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,3 +2,19 @@ export './initialization/init.dart';
 export './settings/settings.dart';
 export './map/map_page.dart';
 export './home/home.dart';
+export 'sensor_page/air_quality_page.dart'
+    hide padding, horizontalOffset, clipSize, verticalOffset, pageOffset;
+export 'sensor_page/door_page.dart';
+export 'sensor_page/energy_data_page.dart';
+export 'sensor_page/flood_data_page.dart';
+export 'sensor_page/ground_humidity_page.dart'
+    hide padding, horizontalOffset, clipSize, verticalOffset, pageOffset;
+export 'sensor_page/parking_sensor_page.dart'
+    hide padding, horizontalOffset, clipSize, verticalOffset, pageOffset;
+export 'sensor_page/person_count_page.dart'
+    hide padding, horizontalOffset, clipSize, verticalOffset, pageOffset;
+export 'sensor_page/raised_garden_page.dart';
+export 'sensor_page/structure_damage_page.dart';
+export 'sensor_page/waste_level_page.dart';
+export 'sensor_page/weather_station_page.dart'
+    hide padding, horizontalOffset, clipSize, verticalOffset, pageOffset;
