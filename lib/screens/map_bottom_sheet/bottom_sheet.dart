3fea76eff7bc4e0d71db5a001c3e57256f3ff47f
mironicagicu <mironicagicu@gmail.com>
2020-11-08 10:43:58 +0100

    [feature-map] Fix UI

--- lib/screens/map_bottom_sheet/bottom_sheet.dart
+++ lib/screens/map_bottom_sheet/bottom_sheet.dart
@@ -1,72 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:lorapark_app/data/models/sensor.dart';
 
-class BottomSheet extends StatefulWidget {
-  BottomSheet({Key key}) : super(key: key);
+class MapBottomSheet extends StatelessWidget {
+  final List<Sensor> sensorList;
 
-  @override
-  _BottomSheetState createState() => _BottomSheetState();
-}
-
-class _BottomSheetState extends State<BottomSheet> {
-  double _initialSheetChildSize = 0.3;
-  double _dragScrollSheetExtent = 0;
-  double _widgetHeight = 0;
-  double _fabPosition = 0;
-  double _fabPositionPadding = 10;
-
-  @override
-  void initState(){
-    super.initState();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      setState(() {
-
-        _fabPosition = _initialSheetChildSize * _widgetHeight;
-      });
-    });
-  }
-
+  MapBottomSheet({this.sensorList});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          color: Colors.pink,
-          // here maps.
-        ),
-        Positioned(
-          bottom: _fabPosition + _fabPositionPadding,
-          right: _fabPositionPadding,
-          child: FloatingActionButton(),
-        ),
-
-
-        NotificationListener<DraggableScrollableNotification>(
-          onNotification: (DraggableScrollableNotification notification) {
-            setState(() {
-              _widgetHeight = context.size.height;
-              _dragScrollSheetExtent = notification.extent;
-
-              // Calculate FAB position based on parent widget height and DraggableScrollable position
-              _fabPosition = _dragScrollSheetExtent * _widgetHeight;
-            });
-          return;
-          },
-          child: DraggableScrollableSheet(
-            initialChildSize: _initialSheetChildSize,
-            maxChildSize: 0.5,
-            minChildSize: 0.1,
-            builder: (context, scrollController) => ListView.builder(
-              controller: scrollController,
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-
-              },
-            ),
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.3,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                blurRadius: 5,
+                spreadRadius: 2,
+                color: Colors.black.withOpacity(0.15))
+          ],
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20),
+            topLeft: Radius.circular(20),
           ),
         ),
-      ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: SizedBox(
+                  height: 3,
+                  width: MediaQuery.of(context).size.width * 0.2,
+                  child: DecoratedBox(
+                    decoration:
+                    BoxDecoration(color: Colors.grey.withOpacity(0.3)),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'Sensors on this Location:',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+            ),
+            ListView.builder(
+              itemCount: sensorList.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                var sensor = sensorList[index];
+                return ListTile(
+                  dense: true,
+                  leading: Text(
+                    '#${sensor.number}',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                  ),
+                  title: Text(
+                    sensor.name,
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                  ),
+                  trailing: Text(
+                    'View ->',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
