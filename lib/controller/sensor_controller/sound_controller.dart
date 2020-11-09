de5be091a059bb9dc50e71bb76a2d365cfe83efc
Tonislav Tachev <tachevv_@abv.bg>
2020-11-10 01:05:50 +0200

    Added rat sensor page, fixed parking sensor page

--- lib/controller/sensor_controller/sound_controller.dart
+++ lib/controller/sensor_controller/sound_controller.dart
@@ -12,45 +12,46 @@ import 'package:lorapark_app/services/logging_service/logging_service.dart';
 import "package:lorapark_app/data/repositories/sensor_repository/sound_sensor.dart";
 import 'package:lorapark_app/config/sensor_list.dart';
 
-enum PAGESTATE {IDLE, LOADED, ERROR}
-class SoundController extends ChangeNotifier{
-    final Logger _logger = GetIt.I.get<LoggingService>().getLogger(( SoundSensorRepository).toString());
-     
-    PAGESTATE pageState = PAGESTATE.IDLE; 
-    
-    SoundSensorRepository _soundRepo;
-      ScrollController  _scrollController = ScrollController();
-    List<SoundSensorData> _data = [];
- 
-   //controller to set the class
-   SoundController({SoundSensorRepository repository}){
-     _soundRepo = repository;
-     fetchData();
-   }
+enum PAGESTATE { IDLE, LOADED, ERROR }
+
+class SoundController extends ChangeNotifier {
+  final Logger _logger = GetIt.I
+      .get<LoggingService>()
+      .getLogger((SoundSensorRepository).toString());
+
+  PAGESTATE pageState = PAGESTATE.IDLE;
+
+  SoundSensorRepository _soundRepo;
+  ScrollController _scrollController = ScrollController();
+  List<SoundSensorData> _data = [];
+
+  //controller to set the class
+  SoundController({SoundSensorRepository repository}) {
+    _soundRepo = repository;
+    fetchData();
+  }
   //function to fetch the data from the backend
-    Future<void> fetchData() async{
-      setData(await _soundRepo.get(id:SensorEndpoints.soundSensor_one));
-       _logger.d('Fetching data');
+  Future<void> fetchData() async {
+    setData(await _soundRepo.get(id: SensorEndpoints.soundSensor_one));
+    _logger.d('Fetching data');
   }
 
-   void setData(List<SoundSensorData> _listData){
-     _data = _listData;
-     notifyListeners();
-      _logger.d('Data fetched');
-   }
+  void setData(List<SoundSensorData> _listData) {
+    _data = _listData;
+    notifyListeners();
+    _logger.d('Data fetched');
+  }
 
-   
-   //get the whole data
-   List<SoundSensorData> get data =>_data;
-   
-   //get fast noises
-   double get fastNoise =>_data.first.firstdbafast;
-   //get continuous noises ===> traffic noises
-   double get continuousNoise => _data.first.firstdbaslow;
-  
-   //get sound pressure
-   double get soundPressure => _data.first.firstleqa;
+  //get the whole data
+  List<SoundSensorData> get data => _data;
 
-   ScrollController get scrollController => _scrollController;
+  //get fast noises
+  double get fastNoise => _data.first.firstdbafast;
+  //get continuous noises ===> traffic noises
+  double get continuousNoise => _data.first.firstdbaslow;
 
+  //get sound pressure
+  double get soundPressure => _data.first.firstleqa;
+
+  ScrollController get scrollController => _scrollController;
 }
