4deb54db5ba8f444c201a8e802224e21b115e77e
Aleksejs Marmiss <aleksejs.marmiss@gmail.com>
2020-11-12 22:32:36 +0100

    [AugmentedReality] AR setup created and Android dependencies defined

--- lib/data/models/coordinates.dart
+++ lib/data/models/coordinates.dart
@@ -12,6 +12,19 @@ class SensorLocation extends Coordinates {
   factory SensorLocation.fromJSON(Map<String, dynamic> json){
     return SensorLocation(double.parse(json['latitude'].toString()), double.parse(json['longitude'].toString()));
   }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) || (other is SensorLocation && runtimeType == other.runtimeType && longitude == other.longitude && latitude == other.latitude);
+  }
+  @override
+  int get hashCode => longitude.hashCode^latitude.hashCode;
+
+  Map<String, dynamic> toJSON()=>{
+    'longitude': longitude,
+    'latitude': latitude,
+
+  };
 }
 
 class UserLocation extends Coordinates {
