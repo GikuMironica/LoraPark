c2867afeaa438e9375abf4647496ca2894a22e63
Tonislav Tachev <tachevv_@abv.bg>
2020-11-15 22:19:55 +0200

    Added sound sensor page

--- lib/screens/sensor_page/sound_sensor_page.dart
+++ lib/screens/sensor_page/sound_sensor_page.dart
@@ -25,35 +25,86 @@ class SoundSensorPage extends StatefulWidget{
 class _SoundSensorPage extends State<SoundSensorPage>{
 
   @override
-  Widget build(BuildContext context) {
-    var _soundSensorController = Provider.of<SoundController>(context);
-     return RefreshIndicator(
-       onRefresh:() async => await _soundSensorController.fetchData(),
-       child: SingleSensorViewTemplate(scrollController: _soundSensorController.scrollController, sliverlist:SliverList(delegate:SliverChildListDelegate([
-      Padding(
-        padding: const EdgeInsets.all(padding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DataPresenter(
-              title: 'Sound pressure', 
-              data:_soundSensorController.data == null ? Text('0%')
-              :Text(_soundSensorController.fastNoise.toString()), 
-              width: MediaQuery.of(context).size.width,
-              height: (MediaQuery.of(context).size.height-250)/4,)
-          ],
-        ),
-      )
-    ]
-    )) , clipSize: clipSize,
-    sensorName: 'Sound Sensor', sensorNumber:'3',)
-     );
-    throw UnimplementedError();
+   Widget build(BuildContext context) {
+    soundController = Provider.of<SoundController>(
+      context,
+      listen: false,
+    );
+    soundController.scrollController.addListener(_scrollListener);
 
-    void _scrollListener() {
+    return RefreshIndicator(
+      onRefresh: () => soundController.fetchData(),
+      child: SingleSensorViewTemplate(
+        scrollController: soundController.scrollController,
+        clipSize: clipSize,
+        sensorName: "Sound sensor",
+        sensorNumber: "2",
+        sliverlist: SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              Padding(
+                padding: const EdgeInsets.all(padding),
+                child: Consumer<SoundController>(
+                    builder: (context, controller, _) => Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            soundController.data.isEmpty 
+                                ? LoadingDataPresenter()
+                                : DataPresenter(
+                                    width: MediaQuery.of(context).size.width,
+                                    height:
+                                        (MediaQuery.of(context).size.height -
+                                                250) /
+                                            4,
+                                    title: "Traffic noise",
+                                    visualization: Image(
+                                      image:
+                                          AssetImage("assets/images/rat.png"),
+                                      height: 200,
+                                      width: 200,
+                                    ),
+                                    data: soundController.data.isEmpty 
+                                        ? Text(
+                                            '0 visits',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w600),
+                                          )
+                                        : Text(
+                                            
+                                                    soundController.continuousNoise.toString() +
+                                                " " +
+                                                "traffic noise",
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                  ),
+                            SizedBox(height: pageOffset / 2),
+
+                            SensorDescription(
+                                text:
+                                    "Mit der ToxProtect bleiben sowohl der Köder als auch das Gift in einer sicheren Box. Die derzeitige Situation wird mit Lichtschranken kontinuierlich überwacht und via LoRaWAN übertragen. Um Ratten anzulocken werden Lockköder eingesetzt, welche die Zahl der täglichen Besuche steigert. Nach einigen Tagen, in denen die Box regelmäßig von Ratten besucht wird, folgt dann der Wechsel zum Giftköder. Nach Wirkung des Gifts, geht die Anzahl an Ratten stetig zurück. Alles wird in Echtzeit überwacht und kann bei Bedarf angepasst werden.",
+                                image: AssetImage(
+                                    "assets/images/rat_sensor.png"))
+
+                                    
+                          ],
+                        )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+   void _scrollListener() {
     setState(() {
-      clipSize = ( _soundSensorController.scrollController.offset / 2);
+      clipSize = (soundController.scrollController.offset / 2);
     });
-   }
   }
 }
