513e1d1ad9cde06328e2dfe47929bab3e72c19cf
krunkerista <rzayev.hidayat@gmail.com>
2020-11-16 22:13:51 +0100

    [ListView] Added 500 ms delay before loading the list of sensors

--- lib/controller/search_controller/sensor_search_controller.dart
+++ lib/controller/search_controller/sensor_search_controller.dart
@@ -1,13 +1,18 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:lorapark_app/controller/search_controller/search_controller.dart';
 import 'package:lorapark_app/data/models/sensor.dart';
 
-class SensorSearchController extends SearchController<Sensor> {
-  List<Sensor> _filteredSensors = [];
+class SensorSearchController extends SearchController {
+  List<Sensor> _allSensors;
+  List<Sensor> _filteredSensors;
 
-  SensorSearchController({@required List<Sensor> sensors})
-      : super(objects: sensors) {
-    _filteredSensors = sensors;
+  SensorSearchController({@required List<Sensor> sensors}) : super() {
+    Future.delayed(Duration(milliseconds: 500), () {
+      _allSensors =
+          sensors.where((sensor) => sensor.number.isNotEmpty).toList();
+      _filteredSensors = _allSensors;
+      notifyListeners();
+    });
   }
 
   List<Sensor> get filteredSensors => _filteredSensors;
@@ -15,26 +20,31 @@ class SensorSearchController extends SearchController<Sensor> {
   @override
   void filter() {
     var originalQuery = textEditingController.text.trim();
+
+    if (originalQuery.isEmpty) {
+      _filteredSensors = _allSensors;
+      notifyListeners();
+      return;
+    }
+
     var query = originalQuery
         .toLowerCase()
         .replaceAll(RegExp('[^a-z0-9 ]'), ' ')
         .trim();
     var keywords = query.split(RegExp(' +'));
 
-    if (originalQuery.isEmpty) {
-      _filteredSensors = allObjects;
-      notifyListeners();
-      return;
-    }
+    _filteredSensors = _filterSensors(keywords);
+    notifyListeners();
+  }
 
-    _filteredSensors = allObjects
+  List<Sensor> _filterSensors(List<String> keywords) {
+    return _allSensors
         .where((sensor) => keywords.length == 1 && keywords.first.isEmpty
             ? false
             : keywords.every((keyword) =>
                 sensor.name.toLowerCase().contains(keyword) ||
+                sensor.description.toLowerCase().contains(keyword) ||
                 sensor.number.contains(keyword)))
         .toList();
-
-    notifyListeners();
   }
 }
